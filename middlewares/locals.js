e68d1047aaaa09b19f7ff9d9fa1884ae4dd0fc26
Wingysam <wingysam+git@gmail.com>
2018-11-23 12:41:08 -0500

    Better flash

--- middlewares/locals.js
+++ middlewares/locals.js
@@ -1,7 +1,5 @@
 const config = require('../config');
 module.exports = (req, res, next) => {
-  res.locals.successes = req.flash('success');
-  res.locals.errors = req.flash('error');
   res.locals.config = config;
   res.locals.req = req;
   next();
