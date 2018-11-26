0a42e69fc223608804a1041b5a5d7fab95ff4a2f
Wingysam <wingysam+git@gmail.com>
2018-11-26 16:18:28 -0500

    Add useCDN option

--- config/index.js
+++ config/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+
+const yesNo = require('yes-no');
+
 module.exports = {
   dbUrl: process.env.DB_URL || 'db',
   defaultFailureRedirect: process.env.DEFAULT_FAILURE_REDIRECT || '/login',
@@ -6,5 +9,6 @@ module.exports = {
   proxyServer: process.env.PROXY_SERVER || undefined,
   secret: process.env.SECRET || require('./secret'),
   sessionMaxAge: Number(process.env.SESSION_MAX_AGE) || 1000 * 60 * 60 * 24 * 7,
-  siteTitle: process.env.SITE_TITLE || 'Christmas Community'
+  siteTitle: process.env.SITE_TITLE || 'Christmas Community',
+  useCDN: yesNo.parse(process.env.USE_CDN || true)
 };
