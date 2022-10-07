2f6ff28f5276e007dbd0acc63df143b209a3f291
Coal <dibashtori@gmail.com>
2022-10-06 18:57:29 -0600

    Restore formatting for config/index.js

--- config/index.js
+++ config/index.js
@@ -1,26 +1,20 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
-    dbPrefix: process.env.DB_PREFIX || "dbs/",
-    defaultFailureRedirect: process.env.DEFAULT_FAILURE_REDIRECT || "/login",
-    port: Number(process.env.PORT) || 3000,
-    dbExposePort: Number(process.env.DB_EXPOSE_PORT) || null,
-    proxyServer: process.env.PROXY_SERVER || undefined,
-    secret: process.env.SECRET || require("./secret"),
-    sessionMaxAge:
-        Number(process.env.SESSION_MAX_AGE) || 1000 * 60 * 60 * 24 * 7,
-    siteTitle: process.env.SITE_TITLE || "Christmas Community",
-    shortTitle: process.env.SHORT_TITLE || "Christmas",
-    wishlist: require("./wishlist"),
-    base: (process.env.ROOT_PATH || "/").endsWith("/")
-        ? process.env.ROOT_PATH || "/"
-        : `${process.env.ROOT_PATH}/`,
-    trustProxy:
-        process.env.TRUST_PROXY === "true"
-            ? true
-            : process.env.TRUST_PROXY || "loopback",
-    bulmaswatch: (process.env.BULMASWATCH || "default").toLowerCase(),
-    pfp: process.env.PFP !== "false",
-    language: process.env.LANGUAGE?.toLowerCase() || "en-us",
-    guestPassword: process.env.GUEST_PASSWORD,
-};
+  dbPrefix: process.env.DB_PREFIX || 'dbs/',
+  defaultFailureRedirect: process.env.DEFAULT_FAILURE_REDIRECT || '/login',
+  port: Number(process.env.PORT) || 3000,
+  dbExposePort: Number(process.env.DB_EXPOSE_PORT) || null,
+  proxyServer: process.env.PROXY_SERVER || undefined,
+  secret: process.env.SECRET || require('./secret'),
+  sessionMaxAge: Number(process.env.SESSION_MAX_AGE) || 1000 * 60 * 60 * 24 * 7,
+  siteTitle: process.env.SITE_TITLE || 'Christmas Community',
+  shortTitle: process.env.SHORT_TITLE || 'Christmas',
+  wishlist: require('./wishlist'),
+  base: (process.env.ROOT_PATH || '/').endsWith('/') ? (process.env.ROOT_PATH || '/') : `${process.env.ROOT_PATH}/`,
+  trustProxy: process.env.TRUST_PROXY === 'true' ? true : process.env.TRUST_PROXY || 'loopback',
+  bulmaswatch: (process.env.BULMASWATCH || 'default').toLowerCase(),
+  pfp: process.env.PFP !== 'false',
+  language: process.env.LANGUAGE?.toLowerCase() || 'en-us',
+  guestPassword: process.env.GUEST_PASSWORD
+}
