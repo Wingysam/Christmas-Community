1eb3ae8f9f45e2fbe3a9be5fe0a9620c6cad3357
Wingy <git@wingysam.xyz>
2023-12-16 15:20:21 -0500

    show valid languages when invalid one is supplied

--- src/Language.ts
+++ src/Language.ts
@@ -1,14 +1,17 @@
+import fs from 'fs/promises'
+
 import * as nativeLanguage from './languages/en-us.js'
 import config from './config/index.js'
 
-let language
-try {
-  language = await import(`./languages/${config.language}.js`)
-} catch (error) {
-  if (error.message.startsWith('Cannot find module')) console.error(`Language ${config.language} is not supported. If you know this language and would like to translate Christmas Community, please ask for help doing so here: https://github.com/Wingysam/Christmas-Community/issues/new`)
-  else console.error(`Failed to load language ${config.language} because of ${error}`)
+const validLanguages = (await fs.readdir('built/languages'))
+  .map(languageFilename => languageFilename.split('.js')[0])
+
+if (!validLanguages.includes(config.language)) {
+  console.error(`Language ${config.language} is not supported. If you know this language and would like to translate Christmas Community, please ask for help doing so here: https://github.com/Wingysam/Christmas-Community/issues/new\nSupported languages: ${validLanguages.join(', ')}`)
   process.exit(1)
 }
+
+const language = await import(`./languages/${config.language}.js`)
 
 if (_CC.moment.locale(language.momentLocale) !== language.momentLocale) {
   console.error(`${_CC.moment.locale()} Failed to load language ${config.language}, moment locale missing. Valid locales: ${_CC.moment.locales().join(', ')}`)
