2a39b149ba4587488e6c9f3f2dabd1350bd2dbcb
Wingy <git@wingysam.xyz>
2025-01-25 15:25:09 -0500

    Remove blank line

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
-
 export default [
   { ignores: ['src/static/libraries/**', 'built/**'] },
   {
